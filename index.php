d6aeeb481940e2bdd5039cafddaa70d40bedcd21
vegizinho <miroslav.vegh@techlab.hr>
2014-09-01 11:50:55 +0200

    Header/footer adjustments

--- index.php
+++ index.php
@@ -1,52 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Techlab Bootstrap Template Prototype</title>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Techlab Bootstrap Template Prototype</title>
 
-    <!-- Bootstrap -->
+<!-- Bootstrap -->
 
-    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
+<link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/main.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
-  <body>    
-    <div class="container">
-    	<div class="header">
-    		<div class="row">
-    			<div class="col-md-12">This is header content here ;-)</div>
-    		</div>
-    	</div>
-    	<div class="menu">
-    		<div class="row">
-    			<div class="col-md-12">This is menu content here ;-)</div>
-    		</div>
-    	</div>
-    	<div class="row">
-    		<div class="col-md-12">
-    		    <h1>Buenos dios muchacos!</h1>
-    			<h3>This is just a test content that will show how we can use Bootstrap cells and containers while resizing our screen.</h3>
-    		</div>
-    		<div class="col-md-3">Just one cell out of 4 in a row!</div>
-    		<div class="col-md-3">Just one cell out of 4 in a row!</div>
-    		<div class="col-md-3">Just one cell out of 4 in a row!</div>
-    		<div class="col-md-3">Just one cell out of 4 in a row!</div>
-    	</div>
-    </div>
-    
-    
+</head>
+<body>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
-  </body>
+	<div class="fullwidth">
+		<div class="container">
+			<div class="header">
+				<div class="row">
+					<div class="col-md-12">This is header content here ;-)</div>
+				</div>
+			</div>
+			<div class="menu">
+				<div class="row">
+					<div class="col-md-12">This is menu content here ;-)</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Buenos dios muchacos!</h1>
+				<h3>This is just a test content that will show how we can use
+					Bootstrap cells and containers while resizing our screen.</h3>
+				<p>Just a simple paragraph text. Just a simple paragraph text. Just
+					a simple paragraph text. Just a simple paragraph text. Just a
+					simple paragraph text. Just a simple paragraph text. Just a simple
+					paragraph text. Just a simple paragraph text. Just a simple
+					paragraph text. Just a simple paragraph text. Just a simple
+					paragraph text.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="fullwidth">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">Footer text</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script
+		src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+</body>
 </html>
