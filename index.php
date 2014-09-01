cf0563109ab7349b54a0b483202d9644ec70bdc9
vegizinho <miroslav.vegh@techlab.hr>
2014-09-01 15:15:52 +0200

    footer align and test

--- index.php
+++ index.php
@@ -21,79 +21,86 @@
 <body>
 
 	<div class="fullwidth">
-	
-	<nav class="navbar navbar-default" role="navigation">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Brand</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Početna</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Proizvodi<span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Luxura X10</a></li>
-            <li><a href="#">Luxura X5</a></li>
-            <li><a href="#">Luxura X3</a></li>
-            <li><a href="#">Luxura V10</a></li>
-            <li><a href="#">Luxura V7</a></li>
-            <li><a href="#">Luxura V5</a></li>
-          </ul>
-        </li>
-        <li class="dropdown"><a href="#">Kontakt</a></li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-	
-	
-	
-	
-	
-	
-<!-- 		<div class="container"> -->
-<!-- 			<div class="header"> -->
-<!-- 				<div class="row"> -->
-<!-- 					<div class="col-md-12">This is header content here ;-)</div> -->
-<!-- 				</div> -->
-<!-- 			</div> -->
-<!-- 			<div class="menu"> -->
-<!-- 				<div class="row"> -->
-<!-- 					<div class="col-md-12">This is menu content here ;-)</div> -->
-<!-- 				</div> -->
-<!-- 			</div> -->
-<!-- 		</div> -->
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed"
+						data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span> <span
+							class="icon-bar"></span> <span class="icon-bar"></span> <span
+							class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Brand</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse"
+					id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Mikro početna</a></li>
+						<li class="dropdown"><a href="#">Luxura početna</a></li>
+						<li class="dropdown"><a href="#" class="dropdown-toggle"
+							data-toggle="dropdown">Proizvodi<span class="caret"></span>
+						</a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Luxura X10</a></li>
+								<li><a href="#">Luxura X5</a></li>
+								<li><a href="#">Luxura X3</a></li>
+								<li><a href="#">Luxura V10</a></li>
+								<li><a href="#">Luxura V7</a></li>
+								<li><a href="#">Luxura V5</a></li>
+							</ul>
+						</li>
+						<li class="dropdown"><a href="#">Kontakt</a></li>
+					</ul>
+					<!--       <form class="navbar-form navbar-left" role="search"> -->
+					<!--         <div class="form-group"> -->
+					<!--           <input type="text" class="form-control" placeholder="Search"> -->
+					<!--         </div> -->
+					<!--         <button type="submit" class="btn btn-default">Submit</button> -->
+					<!--       </form> -->
+
+
+					<!--       <ul class="nav navbar-nav navbar-right"> -->
+					<!--         <li><a href="#">Link</a></li> -->
+					<!--         <li class="dropdown"> -->
+					<!--           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a> -->
+					<!--           <ul class="dropdown-menu" role="menu"> -->
+					<!--             <li><a href="#">Action</a></li> -->
+					<!--             <li><a href="#">Another action</a></li> -->
+					<!--             <li><a href="#">Something else here</a></li> -->
+					<!--             <li class="divider"></li> -->
+					<!--             <li><a href="#">Separated link</a></li> -->
+					<!--           </ul> -->
+					<!--         </li> -->
+					<!--       </ul> -->
+				</div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container-fluid -->
+		</nav>
+
+
+
+
+
+
+		<!-- 		<div class="container"> -->
+		<!-- 			<div class="header"> -->
+		<!-- 				<div class="row"> -->
+		<!-- 					<div class="col-md-12">This is header content here ;-)</div> -->
+		<!-- 				</div> -->
+		<!-- 			</div> -->
+		<!-- 			<div class="menu"> -->
+		<!-- 				<div class="row"> -->
+		<!-- 					<div class="col-md-12">This is menu content here ;-)</div> -->
+		<!-- 				</div> -->
+		<!-- 			</div> -->
+		<!-- 		</div> -->
 	</div>
-		
+
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
@@ -111,9 +118,14 @@
 	</div>
 
 	<div class="fullwidth">
-		<div class="container">
+		<div class="container footer">
 			<div class="row">
-				<div class="col-md-12">Footer text</div>
+				<div class="col-md-12">
+					<strong>Copyright
+					<?php echo date('Y'); ?>
+					. &copy; Mikro d.o.o. Sva prava pridržana.</strong> Zabranjeno kopiranje i
+					prenošenje informacija sa stranice bez prethodne dozvole.
+				</div>
 			</div>
 		</div>
 	</div>
