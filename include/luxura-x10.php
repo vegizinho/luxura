cec295af0bf45e1e587a39fed1b5d06fcb65e55c
vegizinho <miroslav.vegh@techlab.hr>
2014-09-15 12:58:06 +0200

    X10 page fixes
    : Luxura image and css adjustments 
    
    Task-Url:

--- include/luxura-x10.php
+++ include/luxura-x10.php
@@ -1,53 +1,65 @@
 <div class="main container">
 	<div class="row">
-		<div class="col-md-3">Big sunbed image</div>
-		<div class="col-md-6">
-			<h2>Luxura X10, our breathtaking queen</h2>
-			<p>Just a simple paragraph text. Just a simple paragraph text. Just a
-				simple paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text.</p>
-			<h2>Tan like a celebrity</h2>
-			<p>Just a simple paragraph text. Just a simple paragraph text. Just a
-				simple paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text. Just a simple
-				paragraph text. Just a simple paragraph text.</p>
+		<div class="col-md-4">
+			<img class="img-responsive frontpage-position"
+				src="img/frontpage/x10-big.png">
+		</div>
+		<div class="col-md-5">
+			<h2 class="purple">Luxura X10, our breathtaking queen</h2>
+			<p>Luxury features, ultimate comfort, plenty of space and a wonderful
+				tan too...The X10 is the star of our fabulous Luxura product line
+				and is available in the attractive colours Crystal White, Pearl
+				White and Arctic Blue. The design is undoubtedly stunning and
+				eye-catching, but the most striking thing is that this beauty keeps
+				all its promises!</p>
+			<h2 class="purple">Tan like a celebrity</h2>
+			<p>Impressive tanning results are obtained every time with the Luxura
+				X10 thanks to the 46 or 52 tanning tubes and features such as XL
+				Light (2-metre tubes) and SLi+ (four special face tanners). The
+				performance and efficiency of the tubes have been optimised. With IP
+				Control, they are also energy saving and give maximum output during
+				the full lamp life. The comfortable way in which these results are
+				achieved during the tanning session stimulates all the senses of the
+				sun worshipper. The eye-catching design with its sophisticated
+				lighting, fragrant scents, perfect sound quality of your own choice
+				of music, soothing ventilation for the body and face, refreshing
+				mist spray, comfortable lying area, a user-friendly display and if
+				required a voice to guide the user through the menu options -
+				tanning has never been such a wellness experience!</p>
 		</div>
 		<div class="col-md-3">
 			<div class="list-group movedown1">
-				<a href="#" class="list-group-item active">Luxura Model</a> <a
-					href="#" class="list-group-item"><span
+				<a href="?qs=luxura-x10" class="list-group-item active">Luxura X10</a>
+				<a href="#" class="list-group-item"><span
+					class="glyphicon glyphicon-chevron-right moveright1"></span>X10
+					Admoresphere</a> <a href="#" class="list-group-item"><span
 					class="glyphicon glyphicon-chevron-right moveright1"></span>Features</a>
 				<a href="#" class="list-group-item"><span
 					class="glyphicon glyphicon-chevron-right moveright1"></span>Technical
 					data</a> <a href="#" class="list-group-item"><span
 					class="glyphicon glyphicon-chevron-right moveright1"></span>Maintenence</a>
-				<a href="#" class="list-group-item"><span
+				<a href="#" class="list-group-item" data-toggle="collapse"
+					data-parent="#accordion" href="#collapseOne"><span
 					class="glyphicon glyphicon-chevron-right moveright1"></span>Tanning
 					accessories</a> <a href="#" class="list-group-item"><span
+					class="glyphicon glyphicon-hand-right moveright1"></span><span
+					class="glyphicon glyphicon-chevron-right moveright1"></span>Maxlight</a>
+				<a href="#" class="list-group-item"><span
+					class="glyphicon glyphicon-hand-right moveright1"></span><span
+					class="glyphicon glyphicon-chevron-right moveright1"></span>DesinFact</a>
+				<a href="#" class="list-group-item"><span
 					class="glyphicon glyphicon-chevron-right moveright1"></span>Photo
 					gallery</a> <a href="#" class="list-group-item"><span
 					class="glyphicon glyphicon-chevron-right moveright1"></span>Brochures</a>
 			</div>
+			<img class="img-responsive center" src="img/frontpage/x10-logo.png">
 		</div>
 	</div>
 </div>
 
 <div class="main container movedown1">
 	<div class="row subcontent">
-		<div class="col-md-1">
-			
-		</div>
+		<div class="col-md-1"></div>
 		<div class="col-md-2">
 			<p>Image</p>
 		</div>
@@ -58,9 +70,9 @@
 				session. The new full colour display with smart touch control,
 				Ambient FlowLight and a high quality Sound System make you want to
 				use nothing else again. Discover it yourself! Luxura X7 - I like!</p>
-				<div class="btn-group-sm readmoreBTN">
-					<button type="button" class="btn btn-default">Read more</button>
-				</div>
+			<div class="btn-group-sm readmoreBTN">
+				<button type="button" class="btn btn-default">Read more</button>
+			</div>
 		</div>
 		<div class="col-md-2">
 			<p>Image</p>
@@ -72,9 +84,9 @@
 				session into a real experience. In Crystal White the X5 is an eye
 				catcher that fits everywhere. This talent ensures perfect tanning
 				results. Luxura X5 - I like!</p>
-				<div class="btn-group-sm readmoreBTN">
-					<button type="button" class="btn btn-default">Read more</button>
-				</div>
+			<div class="btn-group-sm readmoreBTN">
+				<button type="button" class="btn btn-default">Read more</button>
+			</div>
 		</div>
 	</div>
 </div>
