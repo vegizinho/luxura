269e0c4f7cfb2fe8a06cd227b38c56512647afa2
vegizinho <miroslav.vegh@techlab.hr>
2014-11-16 22:37:39 +0100

    Page updates : Luxura - advanced options on master structure
    Task-Url:

--- include/luxura-x10-highlights-features.php
+++ include/luxura-x10-highlights-features.php
@@ -22,21 +22,100 @@
 <div class="container">
 	<div class="col-md-12">
 			<div class="row">
-				<div class="col-md-6"><h1>Highlights</h1><h2>we like details</h2></div>
-				<div class="col-md-6">Velika slika solarija</div>
+				<div class="col-md-6"><h2>we like</h2><h1>details</h1></div>
+				<div class="col-md-6 movedown"><img class="rightImage" src="img/placeholder.png"></div>
 			</div>
 			
 			<div class="row">
-				<div class="col-md-12"><h2>we like features</h2></div>
-				<div class="col-md-12">Technology</div>
-				<div class="col-md-12">Face Tanners & Filters</div>
-				<div class="col-md-12">Design</div>
-				<div class="col-md-12">Audio</div>
-				<div class="col-md-12">Wellness</div>
+				<div class="col-md-12"><h2 class="color-green">we like</h2><h1 class="color-green">features</h1></div>
+				
+				<div class="col-md-12 bottom-green">
+					<div><h6>TECHNOLOGY</h6></div>
+					<div class="col-md-6">
+					STANDARD FEATURES
+						<ul class="list-normal">
+							<li>XL Light</li>
+							<li>Smart Cooling</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+					OPTIONAL FEATURES
+						<ul class="list-normal">
+							<li>Ambient FlowLight</li>
+							<li>IP Control</li>
+						</ul>
+					</div>
+				</div>
+				
+				<div class="col-md-12 bottom-green">
+					<div><h6>FACE TANNERS & FILTERS</h6></div>
+					<div class="col-md-6">
+					STANDARD FEATURES
+						<ul class="list-normal">
+							<li>SLi+</li>
+							<li>SPS</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+					OPTIONAL FEATURES
+						<ul class="list-normal">
+							<li>HPS</li>
+							<li>BPS</li>
+						</ul>
+					</div>
+				</div>
+				
+				<div class="col-md-12 bottom-green">
+					<div><h6>DESIGN</h6></div>
+					<div class="col-md-6">
+					STANDARD FEATURES
+						<ul class="list-normal">
+							<li>DEcolight</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+					OPTIONAL FEATURES
+						<ul class="list-normal">
+							<li>Admoresphere Edition</li>
+						</ul>
+					</div>
+				</div>
+				
+				<div class="col-md-12 bottom-green">
+					<div><h6>AUDIO</h6></div>
+					<div class="col-md-6">
+					OPTIONAL FEATURES
+						<ul class="list-normal">
+							<li>My MP3</li>
+							<li>SurroundAround Plus</li>
+							<li>SurroundAround Basic</li>
+							<li>Smartvoice</li>
+						</ul>
+					</div>
+				</div>
+				
+				<div class="col-md-12 bottom-green">
+					<div><h6>WELLNESS</h6></div>
+					<div class="col-md-6">
+					STANDARD FEATURES
+						<ul class="list-normal">
+							<li>ContourPlus</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+					OPTIONAL FEATURES
+						<ul class="list-normal">
+							<li>Xsens</li>
+							<li>Qsens</li>
+							<li>CliMax</li>
+						</ul>
+					</div>
+				</div>
+				
 			</div>
 			
 			<div class="row">
-				<div class="col-md-12"><h2>we like it colourful</h2></div>
+				<div class="col-md-12"><h2 class="color-yellow">we like it</h2><h1 class="color-yellow">colourful</h1></div>
 				<div class="col-md-12">Odabir boja i slike</div>
 			</div>
 	</div>	
--- include/luxura-x10-specifications.php
+++ include/luxura-x10-specifications.php
@@ -22,22 +22,156 @@
 <div class="container">
 	<div class="col-md-12">
 			<div class="row">
-				<div><h1>Specification</h1></div>
+				<div class="col-md-8"><h2 class="mega">we like</h2><h1 class="mega">choices</h1></div>
+				<div class="col-md-4 movedown"><img class="rightImage-small" src="img/placeholder.png"></div>
 			</div>
-			
 			
 			<div class="row">
 					<div class="panel panel-default noborder">
 					  <!-- Default panel contents -->
 					  <!-- <div class="panel-heading">Panel heading</div> -->
-					  <div class="panel-body">
-					    <p>...</p>
-					  </div>
 					
 					  <!-- Table -->
-					  <table class="table">
-					    ...
-					  </table>
+					  <div class="table-responsive"> 
+					    <table class="table table-striped table-bordered movedown-big">
+					      <thead>
+					        <tr>
+					          <th></th>
+					          <th>52 SLI</th>
+					          <th>52 SLI INTENSIVE</th>
+					          <th>52 SLI HIGH INTENSIVE</th>
+					          <th>52 SLI IP CONTROL</th>
+					          <th>46 SLI</th>
+					          <th>46 SLI INTENSIVE</th>
+					          <th>46 SLI HIGH INTENSIVE</th>
+					          <th>46 SLI IP CONTROL</th>
+					        </tr>
+					      </thead>
+					      <tbody>
+					        <tr>
+					          <td>Tubes top</td>
+					          <td>30 x 160W</td>
+					          <td>30 x 160W</td>
+					          <td>30 x 160W</td>
+					          <td>30 x 80W-180W</td>
+					          <td>26 x 100W</td>
+					          <td>26 x 160W</td>
+					          <td>26 x 160W</td>
+					          <td>26 x 80W-180W</td>
+					        </tr>
+					        <tr>
+					          <td>Tubes base</td>
+					          <td>22 x 100W</td>
+					          <td>22 x 120W</td>
+					          <td>22 x 180W</td>
+					          <td>22 x 80W-180W</td>
+					          <td>20 x 120W</td>
+					          <td>20 x 120W</td>
+					          <td>20 x 180W</td>
+					          <td>20 x 80W-180W</td>
+					        </tr>
+					        <tr>
+					          <td>Face tanner</td>
+					          <td>4 x 520W</td>
+					          <td>4 x 520W</td>
+					          <td>4 x 520W</td>
+					          <td>4 x 520W</td>
+					          <td>4 x 520W</td>
+					          <td>4 x 520W</td>
+					          <td>4 x 520W</td>
+					          <td>4 x 520W</td>
+					        </tr>
+					        <tr>
+					          <td>Central air exhaust capacity</td>
+					          <td>2800 m3</td>
+					          <td>2800 m3</td>
+					          <td>2800 m3</td>
+					          <td>2800 m3</td>
+					          <td>2800 m3</td>
+					          <td>2800 m3</td>
+					          <td>2800 m3</td>
+					          <td>2800 m3</td>
+					        </tr>
+					        <tr>
+					          <td>Connection</td>
+					          <td>400V/3N~/PE/50Hz</td>
+					          <td>400V/3N~/PE/50Hz</td>
+					          <td>400V/3N~/PE/50Hz</td>
+					          <td>400V/3N~/PE/50Hz</td>
+					          <td>400V/3N~/PE/50Hz</td>
+					          <td>400V/3N~/PE/50Hz</td>
+					          <td>400V/3N~/PE/50Hz</td>
+					          <td>400V/3N~/PE/50Hz</td>
+					        </tr>
+					        <tr>
+					          <td>Power</td>
+					          <td>10,7 kW</td>
+					          <td>12,7 kW</td>
+					          <td>14,8 kW</td>
+					          <td>13,4 kW</td>
+					          <td>8,6 kW</td>
+					          <td>9,9 kW</td>
+					          <td>11,7 kW</td>
+					          <td>12,9 kW</td>
+					        </tr>
+					        <tr>
+					          <td>Power (with Climax)</td>
+					          <td>11,6 kW</td>
+					          <td>13,6 kW</td>
+					          <td>15,6 kW</td>
+					          <td>14,7 kW</td>
+					          <td>9,7 kW</td>
+					          <td>11 kW</td>
+					          <td>12,4 kW</td>
+					          <td>14 kW</td>
+					        </tr>
+					        <tr>
+					          <td>Fuses</td>
+					          <td>3 x 20 A</td>
+					          <td>3 x 25 A</td>
+					          <td>3 x 25 A</td>
+					          <td>3 x 25 A</td>
+					          <td>3 x 16 A</td>
+					          <td>3 x 20 A</td>
+					          <td>3 x 25 A</td>
+					          <td>3 x 25 A</td>
+					        </tr>
+					        <tr>
+					          <td>Fuses (with Climax)</td>
+					          <td>3 x 20 A</td>
+					          <td>3 x 25 A</td>
+					          <td>3 x 25 A</td>
+					          <td>3 x 25 A</td>
+					          <td>3 x 20 A</td>
+					          <td>3 x 20 A</td>
+					          <td>3 x 25 A</td>
+					          <td>3 x 25 A</td>
+					        </tr>
+					        <tr>
+					          <td>Weight</td>
+					          <td>520 kg</td>
+					          <td>530 kg</td>
+					          <td>540 kg</td>
+					          <td>485 kg</td>
+					          <td>510 kg</td>
+					          <td>520 kg</td>
+					          <td>530 kg</td>
+					          <td>480 kg</td>
+					        </tr>
+					        <tr>
+					          <td>Weight (with Climax)</td>
+					          <td>570 kg</td>
+					          <td>580 kg</td>
+					          <td>590 kg </td>
+					          <td>535 kg</td>
+					          <td>560 kg</td>
+					          <td>570 kg</td>
+					          <td>580 kg</td>
+					          <td>530 kg</td>
+					        </tr>
+					      </tbody>
+					    </table>
+					  </div>
 					</div>
 					Download specifications
 			</div>
@@ -45,30 +179,61 @@
 			<div class="row">
 					<div class="panel panel-default noborder">
 					  <!-- Default panel contents -->
-					  <h2>Features</h2>					
-					  <div class="panel-body">
-					    <p>...</p>
-					  </div>
-					
-					  <!-- Table -->
-					  <table class="table">
-					    ...
-					  </table>
+					  <h6>Features</h6>					
+						<div class="col-md-6 feature-list">
+						<h5>STANDARD</h5>
+						<ul>
+							<li>XL Light</li>
+							<li>Smart cooling</li>						
+							<li>Adjustable face tanners (0-1-2-3)</li>
+							<li>8 steps adjustable body cooler</li>
+							<li>8 steps adjustable face cooler</li>
+							<li>Quickstart function</li>
+							<li>Dust filters</li>
+							<li>EasyLift acrylic system bench</li>
+							<li>Liftable bench for easy maintenance</li>
+							<li>LCD timer display</li>
+							<li>Connection for external timer</li>
+							<li>Programmable internal timer</li>
+							<li>Loudspeakers</li>
+							<li>1 hour counter non-resetable</li>
+							<li>1 hour counter resetable</li>
+							<li>SPS</li>
+							<li>Decolight</li>
+							<li>ContourPlus</li>
+						</ul>
+						</div>
+						<div class="col-md-6 feature-list">
+						<h5>OPTIONAL</h5>
+						<ul>
+							<li>Xsens I</li>
+							<li>Xsens III</li>						
+							<li>SLi+ (standard feature for the 52 versions)</li>
+							<li>Air exhaust tower</li>
+							<li>Air exhaust tower with warm air return system</li>
+							<li>Compact air exhaust</li>
+							<li>HPS</li>
+							<li>BPS</li>
+							<li>Admoresphere Edition</li>
+							<li>My MP3</li>
+							<li>SoundAround Plus</li>
+							<li>SoundAround Basic</li>
+							<li>Smartvoice</li>
+							<li>Xsens</li>
+							<li>Qsens</li>
+							<li>CliMax</li>
+						</ul>
+						</div>
 					</div>
 			</div>
 
 			<div class="row">
 					<div class="panel panel-default noborder">
 					  <!-- Default panel contents -->
-					  <h2>Dimensions</h2>					
+					  <h6>Dimensions</h6>					
 					  <div class="panel-body">
-					    <p>...</p>
+					    <p>BLUEPRINTS</p>
 					  </div>
-					
-					  <!-- Table -->
-					  <table class="table">
-					    ...
-					  </table>
 					</div>
 			</div>
 
